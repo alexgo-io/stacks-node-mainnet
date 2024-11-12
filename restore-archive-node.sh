16c8a7ffab5dfca9033a7d3c859b7f12518c09ab
mhatal <100742635+mhatal@users.noreply.github.com>
2024-11-11 22:59:29 -0600

    chore: use stacks 3.0.0.0.2

--- restore-archive-node.sh
+++ restore-archive-node.sh
@@ -9,6 +9,6 @@ if [ -e stacks-node ]; then
   exit 0
 fi
 
-curl https://archive.hiro.so/mainnet/stacks-blockchain/mainnet-stacks-blockchain-3.0.0.0.0-latest.tar.gz | tar -zxv
+curl https://archive.hiro.so/mainnet/stacks-blockchain/mainnet-stacks-blockchain-3.0.0.0.2-latest.tar.gz | tar -zxv
 mkdir stacks-node
 mv mainnet stacks-node/
